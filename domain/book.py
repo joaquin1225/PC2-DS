f12590520235a621f4d55acfe30f98fd74d1a443
bxcowo <a.flores.a@uni.pe>
2026-04-29 16:38:36 -0500

    LOOKOWL-57 feat: Actualizar objeto de dominio para libro.

--- domain/book.py
+++ domain/book.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
+from datetime import date
 
 @dataclass
 class Book:
     uid : str
     title : str
-    author : str
-    category : str
     isbn : str
+    description : str
+    editorial : str
+    publication_date : date
+    cover_url : str
+    language : str
+    author : list[str]
+    category : list[str]
+    page_count : int
     n_copies : int
-
